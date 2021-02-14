a148e7dd197a0538a0a90c284b0d96a556bb17ab
Twan H <huiskenstwan@gmail.com>
2021-02-14 21:08:00 +0100

    test(MathError): Test validation, fixing and throwing

--- tests/check_mode_test.py
+++ tests/check_mode_test.py
@@ -3,7 +3,11 @@ from typing import List
 
 import pytest
 
-from notesystem.modes.check_mode import SeperatorError
+from notesystem.modes.check_mode import SeperatorError, MathError
+
+################################
+# --- TEST SEPERATOR ERROR --- #
+################################
 
 
 @pytest.mark.parametrize(
@@ -13,8 +17,7 @@ from notesystem.modes.check_mode import SeperatorError
         (['-------------------------\n', '\n'], True),
         (['-------------------------\n', '# Hello world\n'], False),
         (['-' * random.randint(3, 50), '\n'], True),
-        (['-' * random.randint(3, 50), '# Hello world\n'], False),
-    ],
+        (['-' * random.randint(3, 50), '# Hello world\n'], False), ],
 )
 def test_seperator_error_validate(test_input: List[str], expected: bool):
     sep_err = SeperatorError()
@@ -37,3 +40,55 @@ def test_seperator_error_throws():
     lines = ['---', '\n', '']
     with pytest.raises(Exception):
         sep_err.validate(lines)
+
+###########################
+# --- TEST MATH ERROR --- #
+###########################
+
+
+@pytest.mark.parametrize(
+    'test_input,expected', [
+        ('There is no math it this line', True),
+        ('There only is correct $math$ in this line', True),
+        ('There is $$invalid$$ math in this line', False),
+        ('There are multiple $$math$$ $$math$$', False),
+        ('There is one $correct$ and one $$wrong$$ math block', False),
+        ('Actual math: $$E=m\\cdot c^2$$', False),
+        ('Actual math: $E=m\\cdot c^2$', True),
+    ],
+)
+def test_math_error_validate(test_input, expected):
+    math_error = MathError()
+    assert math_error.validate([test_input]) == expected
+
+
+@pytest.mark.parametrize(
+    'test_input,expected', [
+        (
+            'There is $$invalid$$ math in this line',
+            'There is $invalid$ math in this line',
+        ),
+        (
+            'There are multiple $$math$$ $$math$$',
+            'There are multiple $math$ $math$',
+        ),
+        (
+            'There is one $correct$ and one $$wrong$$ math block',
+            'There is one $correct$ and one $wrong$ math block',
+        ),
+        ('Actual math: $$E=m\\cdot c^2$$', 'Actual math: $E=m\\cdot c^2$'),
+        # NOTE: This line is valid, but in case that a valid line is marked as
+        # wrong it should not chane anything.
+        ('Actual math: $E=m\\cdot c^2$', 'Actual math: $E=m\\cdot c^2$'),
+    ],
+)
+def test_math_error_fix(test_input, expected):
+    math_error = MathError()
+    assert math_error.fix(test_input) == expected
+
+
+def test_math_error_only_accepts_one_line():
+    math_error = MathError()
+    wrong_input_lines = ['$$math$$', '$$ next line $$']
+    with pytest.raises(Exception):
+        math_error.validate(wrong_input_lines)

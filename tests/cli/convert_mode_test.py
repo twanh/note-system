c145de6b556a0189df65d55935ab0c5230e43187
Twan H <huiskenstwan@gmail.com>
2021-02-24 19:01:58 +0100

    ci: Skip test that does not work in ci

--- tests/cli/convert_mode_test.py
+++ tests/cli/convert_mode_test.py
@@ -89,7 +89,7 @@ def test_convert_file_is_called_when_in_path_is_file(convert_file_mock: Mock):
     )
 
 
-@pytest.mark.skipif('CI' in os.environ, reason='Github Actions does not play well with tmpdirs')
+@pytest.mark.skipif(os.environ.get('CI') == 'true', reason='Github Actions does not play well with tmpdirs')
 def test_convert_file_converts_file(tmpdir: py.path.local):
     """Test that convert file convert the file correctly using pandoc with default GitHub.html5 template"""
     content = """\

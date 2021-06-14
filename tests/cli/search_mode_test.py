6b576ac5cfc745ea11d099b89e488f333cf7aa01
Twan H <huiskenstwan@gmail.com>
2021-06-14 17:52:20 +0200

    add tests

--- tests/cli/search_mode_test.py
+++ tests/cli/search_mode_test.py
@@ -1,7 +1,9 @@
+import os
 from unittest.mock import Mock
 from unittest.mock import patch
 
 from notesystem.modes.base_mode import ModeOptions
+from notesystem.modes.search_mode import SearchMode
 from notesystem.notesystem import main
 
 
@@ -32,3 +34,70 @@ def test_search_mode_called_with_correct_args_tags(start_mock: Mock):
     }
 
     start_mock.assert_called_with(expected_options)
+
+
+def test_split_tag_str_correctly():
+
+    search_mode = SearchMode()
+    tags = ['t1', 't2', 't3', 'ajj']
+    args = {
+        'pattern': 'pattern',
+        'path': 'location',
+        'tag_str': ' '.join(tags),
+        'topic': None,
+        'case_insensitive': False,
+        'title': None,
+    }
+    options: ModeOptions = {
+        'visual': True,
+        'args': args,
+    }
+    try:
+        search_mode.start(options)
+    # It will throw FileNotFoundError because location does not exist
+    # but that does not matter for this test
+    except FileNotFoundError:
+        pass
+
+    assert search_mode.tags == tags
+
+
+def test_allow_no_tag():
+
+    search_mode = SearchMode()
+    args = {
+        'pattern': 'pattern',
+        'path': 'location',
+        'tag_str': None,
+        'topic': None,
+        'case_insensitive': False,
+        'title': None,
+    }
+    options: ModeOptions = {
+        'visual': True,
+        'args': args,
+    }
+    try:
+        search_mode.start(options)
+    # It will throw FileNotFoundError because location does not exist
+    # but that does not matter for this test
+    except FileNotFoundError:
+        pass
+
+    assert search_mode.tags == []
+
+
+@patch('notesystem.modes.search_mode.SearchMode._search_dir')
+def test_search_dir_is_called_when_dir_is_given(mock: Mock):
+
+    main(['search', 'pattern', os.getcwd()])
+
+    mock.assert_called_once_with(os.getcwd())
+
+
+@patch('notesystem.modes.search_mode.SearchMode._search_file')
+def test_search_file_is_called_when_file_is_given(mock: Mock):
+
+    main(['search', 'pattern', __file__])
+
+    mock.assert_called_once_with(__file__)
